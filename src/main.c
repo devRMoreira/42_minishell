949273ae0b712af54ab8db4707e0782712e5d514
Ricardo M. <devrmoreira@gmail.com>
2025-04-28 13:31:52 +0100

    beginning

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <rimagalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 10:38:34 by rimagalh          #+#    #+#             */
-/*   Updated: 2025/04/28 12:00:52 by rimagalh         ###   ########.fr       */
+/*   Updated: 2025/04/28 13:31:40 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,25 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+static int ft_is_space(char c)
+{
+	return (c == 32 || (c >= 9 && c <=13));
+}
 
+static int empty(char *str)
+{
+	int i ;
+
+	if(!str)
+		return (0);
+
+	i = -1;
+	while(ft_is_space(str[++i]))
+	if(str[i] == '\0')
+		return 1;
+
+	return 0;
+}
 
 
 int main(int argc, char **argv)
@@ -28,18 +46,27 @@ int main(int argc, char **argv)
 	{
 		//* get input
 		data.input = readline("cenas > ");
-		printf("%s\n", data.input);
+
+		//* prompt again if empty
+		while(empty(data.input))
+			data.input = readline("cenas > ");
+
+		// printf("%s\n", data.input);
+
+		//*add to RL history
 		add_history(data.input);
+
+		//*print history on input == pwd
 		if(!ft_strncmp(data.input, "pwd", 3))
 		{
-		HIST_ENTRY **history = history_list();
-        if (history)
-        {
-            for (int i = 0; history[i]; i++)
-            {
-                printf("%d: %s\n", i + history_base, history[i]->line);
-            }
-        }
+			HIST_ENTRY **history = history_list();
+    	    if (history)
+			{
+				for (int i = 0; history[i]; i++)
+				{
+					printf("%d: %s\n", i + history_base, history[i]->line);
+				}
+			}
 		}
 
 		// free(data.input);
@@ -56,6 +83,8 @@ int main(int argc, char **argv)
 		// printf("%s\n", data.pwd);
 
 	}
+
+	return 0;
 }
 
 

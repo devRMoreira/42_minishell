ac33a4b0ded391fe2565051ed9aa34f17d488b52
Ricardo M. <devrmoreira@gmail.com>
2025-05-20 16:25:05 +0100

    removed old_pwd from data to always use env vars and pwd init

--- src/memory/data.c
+++ src/memory/data.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <rimagalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 17:37:57 by rimagalh          #+#    #+#             */
-/*   Updated: 2025/05/07 15:57:46 by rimagalh         ###   ########.fr       */
+/*   Updated: 2025/05/20 16:11:01 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ static char ** get_envp(char **envp)
 t_data *ft_init_data(char **envp)
 {
 	t_data *data = malloc(sizeof(t_data));
+	char *pwd;
 	if(!data)
 		return (NULL);
 
@@ -55,7 +56,16 @@ t_data *ft_init_data(char **envp)
 		free(data);
 		return NULL;
 	}
-
+	//added this to avoid any non existing pwd nonsense
+	if(!ft_get_env(data, "PWD"))
+	{
+		pwd = getcwd(NULL, 0);
+		if(pwd)
+		{
+			ft_set_env(data, "PWD", pwd);
+			free(pwd);
+		}
+	}
 	data->exit_status = 0;
 	data->tokens = NULL;
 	data->cmds = NULL;
